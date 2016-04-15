4e3f31e2e8503d143deb745f5ad85d75510d22f1
白渐 <jianbai@creditease.cn>
2016-04-15 18:30:41 +0800

    Fix prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,12 +2,11 @@
 set -e
 
 image="mysql/mysql-server:5.6.29"
-no_gtid_cluster="1 2 3"
-with_gtid_cluster="4 5 6"
+cluster="1 2 3"
 
 docker pull $image
 
-for i in $no_gtid_cluster
+for i in $cluster
 do
     docker run --name mysql_$i -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -p 330$i:3306 -d $image --log-bin=binlog --read-only=1 --server-id=$i
 done
@@ -16,7 +15,7 @@ sleep 15
 
 docker ps -a
 
-for i in $no_gtid_cluster
+for i in $cluster
 do
     mysql -uroot -h127.0.0.1 -P330$i < prepare.sql
 done
